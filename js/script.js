ce6e341823e7860f6e04b4d603c66650f66cac16
HOKER1597 <pushyn.son@gmail.com>
2022-02-15 16:17:10 +0200

    Wexom

---
+++ js/script.js
@@ -0,0 +1,94 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+
+$(document).ready(function() {
+    $('.photo-content:first').show(0);
+    $('.photo:first>span').addClass('selected');
+    $('.photo>span').click(function() {
+        $('.tphotoab>span').removeClass('selected');
+        $(this).addClass('selected');
+        $('.photo-content').hide(0);
+        $(this).next('.photo-content').show(0);
+    });
+});
+
+var acc = document.getElementsByClassName("security");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+var acc = document.getElementsByClassName("delivery");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+var acc = document.getElementsByClassName("payment");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+window.addEventListener("DOMContentLoaded", function() {
+    function setCursorPosition(pos, elem) {
+        elem.focus();
+        if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
+        else if (elem.createTextRange) {
+            var range = elem.createTextRange();
+            range.collapse(true);
+            range.moveEnd("character", pos);
+            range.moveStart("character", pos);
+            range.select()
+        }
+    }
+
+    var inputsTel = document.querySelectorAll('input[type="tel"]');
+
+    Inputmask({
+        "mask": "+38(099) 999-99-99",
+        showMaskOnHover: false
+    }).mask(inputsTel);
+});
